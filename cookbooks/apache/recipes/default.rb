9b7ce47064e0adcc7fb8f4d6cac37aa175f76d3d
mmahadeokar <manishmahadeokar@gmail.com>
2018-07-02 00:42:51 +0000

    updated motd file recipe

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,13 +5,21 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 # Installing apache 2.0
 
+
+if node['platform_family'] == "rhel"
+	package = "httpd"
+elsif node['platform_family'] == "debian"
+	package = "apache2"
+end
+
 package 'apache2' do
-	package_name 'httpd'
+	package_name package
 	action :install
 end
 
 service 'apache2' do
-	service_name 'httpd'
+	service_name package
 	action [:start, :enable]
 end
+
 
