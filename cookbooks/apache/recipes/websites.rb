ac9f2de29334e4d618b4855764c90e6bb2ec9854
mmahadeokar <manishmahadeokar@gmail.com>
2018-07-08 18:07:11 +0000

    apache 2.0.0

--- cookbooks/apache/recipes/websites.rb
+++ cookbooks/apache/recipes/websites.rb
@@ -1,4 +1,4 @@
 file 'html www' do
 	path '/var/www/html/index.html'
-	content 'Hello World!, This is Manish Mahadeokar Version 1.0.0'
+	content 'Hello World!, This is Manish Mahadeokar Version 2.0.0'
 end
